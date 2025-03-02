4998da7ebefb1fd3ec4fea710c5f1fb376ba4f6f
HHZKYO <1692189552@qq.com>
2025-03-02 09:56:58 +0800

    three.js立方体

---
+++ src/02.三要素.vue
@@ -0,0 +1,25 @@
+<template></template>
+
+<script setup>
+// 1.引入 three.js
+// * as 收集所有 export 命名导出的属性和方法报保存到 THREE 变量中
+import * as THREE from 'three';
+
+// 2.要素1：创建场景（容器/世界）
+const scene = new THREE.Scene();
+//3.要素2：创建摄像机
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// 4.要素3：创建渲染器
+const renderer = new THREE.WebGLRenderer();
+// 给渲染器设置画布宽高
+renderer.setSize( window.innerWidth, window.innerHeight );
+// renderer.domElement: canvas 画布标签添加到真实 DOM 上，才能看到
+// canvas标签（画布标签）：可以用代码决定绘制的画面效果
+document.body.appendChild( renderer.domElement );
+// 把场景和摄像机传递给渲染器，开始在 canvas 画布上渲染计算后的画面
+renderer.render( scene, camera );
+
+</script>
+
+<style scoped></style>
--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template></template>
 
 <script setup>
-// 1.引入 three.js
-// * as 收集所有 export 命名导出的属性和方法报保存到 THREE 变量中
 import * as THREE from 'three';
 
-// 2.要素1：创建场景（容器/世界）
 const scene = new THREE.Scene();
-//3.要素2：创建摄像机
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-// 4.要素3：创建渲染器
 const renderer = new THREE.WebGLRenderer();
-// 给渲染器设置画布宽高
 renderer.setSize( window.innerWidth, window.innerHeight );
-// renderer.domElement: canvas 画布标签添加到真实 DOM 上，才能看到
-// canvas标签（画布标签）：可以用代码决定绘制的画面效果
 document.body.appendChild( renderer.domElement );
-// 把场景和摄像机传递给渲染器，开始在 canvas 画布上渲染计算后的画面
-renderer.render( scene, camera );
 
+// 目标：添加一个立方体
+// BoxGeometry 立方缓冲集合体（宽度、高度、深度）
+// three.js 单位（米/厘米）
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// MeshBasicMaterial 基础网络材质（简单平面着色）
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// Mesh：网格物体（三角形围出来的平面）
+const cube = new THREE.Mesh( geometry, material );
+// 把立方体加入场景中
+scene.add( cube );
+
+camera.position.z = 5;
+
+renderer.render( scene, camera );
 </script>
 
 <style scoped></style>
