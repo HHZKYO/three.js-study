85ebdcfe30dcdaf46ca46da0dbe2813f34f97291
HHZKYO <1692189552@qq.com>
2025-03-04 12:05:26 +0800

    练习：球体绘制

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,36 @@
 <template></template>
 
 <script setup>
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from 'three'; // 步骤1.三要素
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'; // 步骤3.引入轨道控制器
+const scene = new THREE.Scene(); // 步骤1.三要素
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); // 步骤1.三要素
+camera.position.set(0, 0, 50) // 步骤5.设置相机位置
+const renderer = new THREE.WebGLRenderer(); // 步骤1.三要素
+renderer.setSize( window.innerWidth, window.innerHeight ); // 步骤1.三要素
+document.body.appendChild( renderer.domElement ); // 步骤1.三要素
+const controls = new OrbitControls( camera, renderer.domElement ); // 步骤3
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-const controls = new OrbitControls( camera, renderer.domElement );
+const geometry = new THREE.SphereGeometry( 15, 32, 16 ); // 步骤4.放入物体
+const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } ); // 步骤4.放入物体
+const sphere = new THREE.Mesh( geometry, material ); // 步骤4.放入物体
+scene.add( sphere );
 
-// 目标：控制物体的变换
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// 步骤6.适配
+window.addEventListener('resize', () => {
+  // 画布的最新宽高
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // 画布的最新宽高比
+  camera.aspect = window.innerWidth / window.innerHeight;
+  // 强制更新摄像机的计算矩阵
+  camera.updateProjectionMatrix()
+})
 
-// 1.位移
-// cube.position 值是一个 Vector3 的三维向量对象
-cube.position.set(5, 0, 0)
-
-// 3.缩放
-cube.scale.set(2, 1, 1)
-
-camera.position.set( 0, 2, 10 );
-
-const axesHelper = new THREE.AxesHelper( 5 );
-scene.add( axesHelper );
-
+// 步骤2.准备渲染循环的递归函数
 function animate() {
-  requestAnimationFrame( animate );
-  controls.update();
-  renderer.render( scene, camera );
-  // 2.旋转
-  cube.rotation.x += 0.01
+  renderer.render( scene, camera ); // 步骤2.准备渲染循环的递归函数
+  controls.update(); // 步骤3
+  requestAnimationFrame( animate ); // 步骤2.准备渲染循环的递归函数
 }
 animate()
 </script>
