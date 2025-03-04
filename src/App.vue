fe8d5416aed93518bfe1ac2424cbdea0d21a5c58
HHZKYO <1692189552@qq.com>
2025-03-04 16:44:15 +0800

    汽车展厅-glb模型加载

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
 <script setup>
 import * as THREE from 'three'; 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// 目标：把glb模型文件加载到网页中
+// 1. 引入 GLTFLoader 加载器
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+// 2. 实例化加载器对象
+const gltfLoader = new GLTFLoader();
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -15,6 +21,20 @@ const axesHelper = new THREE.AxesHelper( 5 )
 scene.add( axesHelper )
 
 const controls = new OrbitControls(camera, renderer.domElement)
+
+// 3. 加载 glb 模型对象
+gltfLoader.load(
+  new URL("@/assets/glb/lbgn.glb", import.meta.url).href,
+  (gltf) => {
+    // 4. gltf.scene 才能拿到模型数据对象，添加到场景中
+    scene.add(gltf.scene);
+
+    // 问题1：模型黑色看不到，模型本身没有自发光，我们需要添加一个环境光照亮所有受到光照影响的物体
+    // 环境光
+    const light = new THREE.AmbientLight(0xffffff, 2);
+    scene.add(light);
+  }
+);
 
 function renderLoop() {
   renderer.render(scene, camera)
