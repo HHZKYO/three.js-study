3f55d4eb6469fb76a30ac6f86a023507bd2db052
HHZKYO <1692189552@qq.com>
2025-03-05 09:50:45 +0800

    汽车展厅-车门开关交互

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 <script setup>
 import * as THREE from 'three'; 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import gsap from 'gsap';
 // 目标：把glb模型文件加载到网页中
 // 1. 引入 GLTFLoader 加载器
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
@@ -27,12 +28,52 @@ gltfLoader.load(
   new URL("@/assets/glb/lbgn.glb", import.meta.url).href,
   (gltf) => {
     // 4. gltf.scene 才能拿到模型数据对象，添加到场景中
-    scene.add(gltf.scene);
+    const car3d = gltf.scene
+    scene.add(car3d);
 
     // 问题1：模型黑色看不到，模型本身没有自发光，我们需要添加一个环境光照亮所有受到光照影响的物体
     // 环境光
     const light = new THREE.AmbientLight(0xffffff, 2);
     scene.add(light);
+    // 创建光线投射对象
+    const raycaster = new THREE.Raycaster();
+    // 初始化二维坐标点（空）
+    const pointer = new THREE.Vector2();
+    // 目标：交互绑定
+    // Empty001_16 -> Object_64
+    const leftDoor = car3d.getObjectByName("Empty001_16");
+    // Empty002_20 -> Object_77
+    const rightDoor = car3d.getObjectByName("Empty002_20");
+    function onPointerMove( event ) {
+      pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+      pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+      raycaster.setFromCamera( pointer, camera );
+      const intersects = raycaster.intersectObjects( car3d.children );
+      console.log(intersects);
+      if (intersects.length > 0) {
+        const target = intersects[0].object
+        console.log(target)
+        if (target.name === 'Object_64') {
+          // 左车门，车门原点不在车角落处，角落是另一个父级的父级组物体的原点
+          // userData.自定义属性
+          if (!target.userData.isOpen) {
+            // 打开
+            gsap.to(leftDoor.rotation, {
+              x: Math.PI / 4,
+              duration: 2,
+            })
+          } else {
+            // 关闭
+            gsap.to(leftDoor.rotation, {
+              x: 0,
+              duration: 2,
+            })
+          }
+          target.userData.isOpen = !target.userData.isOpen
+        }
+      }
+    }
+    window.addEventListener( 'click', onPointerMove );
   }
 );
 
